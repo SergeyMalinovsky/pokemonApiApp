e07c1b54e3162ede1e54d6a5c9f985c51a78ca06
Shesterikov Dmitry <dmitry@beamsolutions.com>
2020-01-22 15:28:57 +0300

    Added addittional props to Card component, buttons "View image", "add to favorite"

--- src/containers/Card/index.js
+++ src/containers/Card/index.js
@@ -9,23 +9,49 @@ class Card extends React.Component {
         super(props);
 
         this.state = {
-            isSelectFavorite: false
+            isFavorite: false,
+            isSelectFavorite: false,
+            isShowingImage: false
+        }
+
+        console.log(props);
+    }
+
+    onMouseOverSelectFavoriteImage = state => this.setState({ isSelectFavorite: state });
+    showingPokemonImage = state => this.setState({ isShowingImage: state });
+
+    handleClickOnFavorite = () => {
+        if(!this.state.isFavorite) {
+            this.addToFavorite();
+        } else {
+            this.deleteFromFavorite();
         }
     }
 
-    onMouseOverSelectFavoriteImage = (state) => {
-        this.setState({ isSelectFavorite: state });
-    }
+    addToFavorite = () => this.setState({ isFavorite: true });
+    deleteFromFavorite = () => this.setState({ isFavorite: false });
 
     render() {
         const pokemon = this.props.pokemon;
 
         return (
-            <div className={styles.main}>
+            <div className={styles.main} onMouseOut={() => this.showingPokemonImage(false)}>
+                { this.state.isShowingImage ? 
+                <div className={styles.pokemonImage} onClick={() => this.showingPokemonImage(false)}>
+                    <img 
+                        src={pokemon.imageUrl.front_default} 
+                        alt='pokemonImage'
+                    />
+                </div>
+                :
                 <div className={styles.wrapper}>
                     <div className={styles.top}>
                         <div className={styles.avatar}>
-                            <img src={pokemon.imageUrl.front_default} alt='avatar'/>
+                            <img 
+                                src={pokemon.imageUrl.front_default} 
+                                alt='avatar' 
+                                onClick={() => this.showingPokemonImage(true)}
+                            />
                         </div>
                         <div className={styles.description}>
                             <div className={styles.name}><p>{pokemon.name}</p></div>
@@ -39,16 +65,26 @@ class Card extends React.Component {
                         </div>
                         <div className={styles.favoriteImage}>
                             <img 
-                                src={this.state.isSelectFavorite ? starFilledImage : starEmptyImage}
+                                src={this.state.isSelectFavorite || this.state.isFavorite 
+                                    ? starFilledImage 
+                                    : starEmptyImage
+                                }
+                                
                                 alt='select as favorite'
                                 onMouseOver={() => this.onMouseOverSelectFavoriteImage(true)}
                                 onMouseOut={() => this.onMouseOverSelectFavoriteImage(false)}
+                                onClick={() => this.handleClickOnFavorite()}
                             />
                         </div>
                     </div>
-                    <div className={styles.center}></div>
+                    <div className={styles.center}>
+                        <div className={styles.abilities}>
+                                <span>Abilities:</span><p>{pokemon.abilities.length}</p>
+                        </div>
+                    </div>
                     <div className={styles.bottom}></div>
                 </div>
+                }
             </div>
         );
     }
